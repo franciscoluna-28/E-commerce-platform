b8e74ba8f95e174ba14d4fff91fc166d455f33d1
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-09 21:28:29 -0400

    Created basic linlks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import Products from "~/components/products";
+import Link from 'next/link';
+
 
 
 const Home: NextPage = () => {
-
-
   return (
     <>
       <Head>
@@ -13,7 +14,19 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-<h1 className="text-white font-bold text-5xl">Hola</h1>
+<h1 className="text-white font-bold text-5xl">E-Commerce</h1>
+<Products/>
+<header className="">
+        <ul className="flex justify-center gap-4">
+          <Link className="text-white font-semibold" href="/">Home
+</Link>
+          <Link className="text-white font-semibold" href="/add-product">Add Product
+</Link>
+
+          
+
+        </ul>
+      </header>
       </main>
     </>
   );
