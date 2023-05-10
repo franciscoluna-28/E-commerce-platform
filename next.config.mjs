eba591c63c34aee628f0b9e1fa14cdf820c03e3d
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-09 21:30:22 -0400

    Changed provider for the images to Pexels

--- next.config.mjs
+++ next.config.mjs
@@ -2,21 +2,28 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
+await
+import ("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+    /**
+     * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+     * out.
+     *
+     * @see https://github.com/vercel/next.js/issues/41980
+     */
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+
+    },
+    images: {
+        //TODO change the domains accepted only to a host for uploading images
+        // And Pixabay or Pexels
+        domains: ["images.pexels.com"],
+    },
 };
 export default config;
